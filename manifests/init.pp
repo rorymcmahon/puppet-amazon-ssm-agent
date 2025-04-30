737c6369f5746e99897404c25336493eab7823f2
rory.mcmahon <rory.mcmahon@vocus.com.au>
2025-04-30 03:24:01 +0000

    add package download install functions

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,25 +6,43 @@
 # Parameters
 # ----------
 #
-# * `region`
+# @param activation_code
+# Data Type: String
+# The activation code for the SSM agent. This is used to register the instance with SSM.
+# Default value: undef
+#
+# @param activation_id
+# Data Type: String
+# The activation ID for the SSM agent. This is used to register the instance with SSM.
+# Default value: undef
+# @param region
 # Data Type: String
 # The AWS region from where the package will be downloaded
 # Default value: us-east-1
 #
-# * `proxy_url`
+# @param proxy_url
 # Data Type: String
 # The proxy URL in <protocol>://<host>:<port> format, specify if the ssm agent needs to communicate via a proxy
 # Default value: undef
 #
-# * `service_enable`
+# @param service_enable
 # Data Type: Boolean
 # Ensure state of the service. Can be 'running', 'stopped', true, or false
 # Default value: 'running'
 #
-# * `service_ensure`
+# @param service_ensure
 # Data Type: String, Boolean
 # Whether to enable the service.
 # Default value: true
+#
+# @param tmp_dir
+# Data Type: String
+# The directory to download the package to. 
+# Default value: '/tmp'
+#
+# @param register
+# Data Type: Boolean
+# Whether to register the instance with SSM.
 #
 #
 # Examples
@@ -39,70 +57,85 @@
 # -------
 #
 # Andy Wang <andy.wang@shinesolutions.com>
+# Rory McMahon <rory.mcmahon@vocus.com.au>
 #
 # Copyright
 # ---------
 #
-# Copyright 2017-2019 Shine Solutions, unless otherwise noted.
+# Copyright 2017-2019 Shine Solutions, forked by Vocus Group 
 #
 class amazon_ssm_agent (
+  String $activation_code,
+  String $activation_id,
   String $region              = 'us-east-1',
   Optional[String] $proxy_url = undef,
   Boolean $service_enable     = true,
-  $service_ensure             = 'running',
-  ) {
+  String $service_ensure      = 'running',
+  String $tmp_dir            = '/tmp',
+  Boolean $register           = false,
+) {
+  $pkg_provider = lookup('amazon_ssm_agent::pkg_provider', String, 'first')
+  $pkg_format   = lookup('amazon_ssm_agent::pkg_format', String, 'first')
+  $flavor       = lookup('amazon_ssm_agent::flavor', String, 'first')
 
-    $pkg_provider = lookup('amazon_ssm_agent::pkg_provider', String, 'first')
-    $pkg_format   = lookup('amazon_ssm_agent::pkg_format', String, 'first')
-    $flavor       = lookup('amazon_ssm_agent::flavor', String, 'first')
+  $srv_provider = lookup('amazon_ssm_agent::srv_provider', String, 'first')
 
-    $srv_provider = lookup('amazon_ssm_agent::srv_provider', String, 'first')
+  case $facts['os']['architecture'] {
+    'x86_64','amd64': {
+      $architecture = 'amd64'
+    }
+    'i386': {
+      $architecture = '386'
+    }
+    'aarch64','arm64': {
+      $architecture = 'arm64'
+    }
+    default: {
+      fail("Module not supported on ${facts['os']['architecture']} architecture")
+    }
+  }
 
-    case $facts['os']['architecture'] {
-      'x86_64','amd64': {
-        $architecture = 'amd64'
-      }
-      'i386': {
-        $architecture = '386'
-      }
-      'aarch64','arm64': {
-        $architecture = 'arm64'
-      }
-      default: {
-        fail("Module not supported on ${facts['os']['architecture']} architecture")
-      }
+  # Extract the package to the tmp dir and install it
+  archive { "${tmp_dir}/amazon-ssm-agent.${pkg_format}":
+    ensure  => present,
+    extract => false,
+    cleanup => false,
+    source  => "https://amazon-ssm-${region}.s3.amazonaws.com/latest/${flavor}_${architecture}/amazon-ssm-agent.${pkg_format}",
+    creates => "${tmp_dir}/amazon-ssm-agent.${pkg_format}",
+  } -> package { 'amazon-ssm-agent':
+    ensure   => latest,
+    provider => $pkg_provider,
+    source   => "${tmp_dir}/amazon-ssm-agent.${pkg_format}",
+  }
+
+  if $register {
+    exec { 'register-ssm-agent':
+      command     => "amazon-ssm-agent -register -activation-code ${activation_code} -activation-id ${activation_id} -region ${region}",
+      path        => ['/bin', '/usr/bin'],
+      refreshonly => true,
+      subscribe   => Package['amazon-ssm-agent'],
+    }
+  }
+
+  if $service_ensure {
+    class { '::amazon_ssm_agent::proxy':
+      proxy_url    => $proxy_url,
+      srv_provider => $srv_provider,
+      require      => Package['amazon-ssm-agent'],
     }
 
-    archive {"/tmp/amazon-ssm-agent.${pkg_format}":
-      ensure  => present,
-      extract => false,
-      cleanup => false,
-      source  => "https://amazon-ssm-${region}.s3.amazonaws.com/latest/${flavor}_${architecture}/amazon-ssm-agent.${pkg_format}",
-      creates => "/tmp/amazon-ssm-agent.${pkg_format}",
-    } -> package { 'amazon-ssm-agent':
-      ensure   => latest,
-      provider => $pkg_provider,
-      source   => "/tmp/amazon-ssm-agent.${pkg_format}",
+    service { 'amazon-ssm-agent':
+      ensure   => $service_ensure,
+      enable   => $service_enable,
+      provider => $srv_provider,
     }
 
-    if $service_ensure {
-      class { '::amazon_ssm_agent::proxy':
-        proxy_url    => $proxy_url,
-        srv_provider => $srv_provider,
-        require      => Package['amazon-ssm-agent'],
-      }
+    Class['::amazon_ssm_agent::proxy'] -> Service['amazon-ssm-agent']
+  }
 
-      service { 'amazon-ssm-agent':
-        ensure   => $service_ensure,
-        enable   => $service_enable,
-        provider => $srv_provider,
-      }
-
-      Class['::amazon_ssm_agent::proxy'] -> Service['amazon-ssm-agent']
-    }
-
-    file {"/tmp/amazon-ssm-agent.${pkg_format}":
-      ensure  => absent,
-      require => Package['amazon-ssm-agent'],
-    }
+  # Cleanup the agent package 
+  file { "${tmp_dir}/amazon-ssm-agent.${pkg_format}":
+    ensure  => absent,
+    require => Package['amazon-ssm-agent'],
+  }
 }
--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -6,12 +6,12 @@
 # Parameters
 # ----------
 ##
-# * `proxy_url`
+# @param proxy_url
 # Data Type: String
 # The proxy URL in <protocol>://<host>:<port> format, specify if the ssm agent needs to communicate via a proxy
 # Default value: undef
 #
-# * `srv_provider`
+# @param srv_provider
 # Data Type: srv_provider
 # The name of the service provider
 #
@@ -35,51 +35,51 @@
 # Copyright 2017-2019 Shine Solutions, unless otherwise noted.
 #
 class amazon_ssm_agent::proxy (
-  $proxy_url    = undef,
-  $srv_provider = lookup('amazon_ssm_agent::srv_provider', String, 'first'),
-  ) {
-    $proxy_env_vars = [ 'http_proxy', 'https_proxy', 'HTTP_PROXY', 'HTTPS_PROXY' ]
+  String $proxy_url    = undef,
+  String $srv_provider = lookup('amazon_ssm_agent::srv_provider', String, 'first'),
+) {
+  $proxy_env_vars = ['http_proxy', 'https_proxy', 'HTTP_PROXY', 'HTTPS_PROXY']
 
-    case $srv_provider {
-      'upstart': {
-        $config_file = '/etc/init/amazon-ssm-agent.conf'
-        fail("Module support for ${srv_provider} service provider has been temporarily disabled")
+  case $srv_provider {
+    'upstart': {
+      $config_file = '/etc/init/amazon-ssm-agent.conf'
+      fail("Module support for ${srv_provider} service provider has been temporarily disabled")
+    }
+    'systemd': {
+      $config_file = '/etc/systemd/system/amazon-ssm-agent.service'
+      if $proxy_url {
+        $status = present
+      } else {
+        $status = absent
       }
-      'systemd': {
-        $config_file = '/etc/systemd/system/amazon-ssm-agent.service'
-        if $proxy_url {
-          $status = present
-        } else {
-          $status = absent
-        }
-        # Since puppetlabs/inifile module doesn't support repeat setting,
-        # we have to trick it into thinking that the setting includes the env var.
-        # Once puppetlabs/inifile supports repeat setting, it can be reverted back
-        # to 'Environment'
-        $proxy_env_vars.each |Integer $index, String $proxy_env_var| {
-          ini_setting { "Set proxy configuration ${proxy_env_var} with status ${status}":
-            ensure            => $status,
-            path              => $config_file,
-            section           => 'Service',
-            setting           => "Environment=\"${proxy_env_var}",
-            value             => "${proxy_url}\"",
-            key_val_separator => '=',
-            before            => Service['amazon-ssm-agent'],
-          }
-        }
-        ini_setting { "Set no_proxy configuration with status ${status}":
+      # Since puppetlabs/inifile module doesn't support repeat setting,
+      # we have to trick it into thinking that the setting includes the env var.
+      # Once puppetlabs/inifile supports repeat setting, it can be reverted back
+      # to 'Environment'
+      $proxy_env_vars.each |Integer $index, String $proxy_env_var| {
+        ini_setting { "Set proxy configuration ${proxy_env_var} with status ${status}":
           ensure            => $status,
           path              => $config_file,
           section           => 'Service',
-          setting           => "Environment=\"no_proxy",
-          value             => "169.254.169.254,127.0.0.1,localhost\"",
+          setting           => "Environment=\"${proxy_env_var}",
+          value             => "${proxy_url}\"",
           key_val_separator => '=',
           before            => Service['amazon-ssm-agent'],
         }
       }
-      'none': {}
-      default: {
-        fail("Module does not support ${srv_provider} service provider")
+      ini_setting { "Set no_proxy configuration with status ${status}":
+        ensure            => $status,
+        path              => $config_file,
+        section           => 'Service',
+        setting           => "Environment=\"no_proxy",
+        value             => "169.254.169.254,127.0.0.1,localhost\"",
+        key_val_separator => '=',
+        before            => Service['amazon-ssm-agent'],
       }
+    }
+    'none': {}
+    default: {
+      fail("Module does not support ${srv_provider} service provider")
+    }
   }
 }
